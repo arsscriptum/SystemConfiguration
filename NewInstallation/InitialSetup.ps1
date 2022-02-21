da69419394dc39fc3d6b896de809e5d6f4ad2e92
Guillaume Plante <guillaumeplante.qc@gmail.com>
2022-02-20 19:50:13 -0500

    NewInstall script

---
+++ NewInstallation/InitialSetup.ps1
@@ -0,0 +1,560 @@
+
+<#
+  ╓──────────────────────────────────────────────────────────────────────────────────────
+  ║   PowerShell Script to configure a newly installed Windows system
+  ╙──────────────────────────────────────────────────────────────────────────────────────
+ #>
+
+<#
+
+    1) Set the PowerShell HOME path, the modules path and the module development path
+    2) Set the REGISTRY variable representing the root path of all scripts settings
+    3) Set the System Environment variables...
+
+
+Requirements:
+    1) Github Authentication Token
+
+#>
+
+<#
+.SYNOPSIS
+    A brief description of the function or script.
+    This keyword can be used only once in each topic.
+
+.DESCRIPTION
+    A detailed description of the function or script.
+    This keyword can be used only once in each topic.
+
+.NOTES
+    Some additional notes. This keyword can be used only once in each topic.
+    This keyword can be used only once in each topic.
+
+.LINK
+    A link used when Get-Help with a switch -OnLine is used.
+    This keyword can be used only once in each topic.
+
+.EXAMPLE
+    Example 1
+    You can use this keyword as many as you want.
+
+.EXAMPLE
+    Example 2
+    You can use this keyword as many as you want.
+#>
+
+<#
+    .Synopsis
+        Convert a compressed script in BASE64 string value back in CLEAR
+    .Description
+        Convert a compressed script in BASE64 string value back in CLEAR
+    .Parameter ScriptBlock
+        The ScriptBlock to convert
+    .Inputs
+        ScriptBlock
+    .Outputs
+        ScriptBlock
+#>
+
+
+
+
+#Requires -RunAsAdministrator
+
+
+
+# ============================================================================================================
+# SCRIPT VARIABLES
+# ============================================================================================================
+
+$Script:DEVDRIVE               = 'P:\'
+$Script:DEV_ROOT               = 'P:\Development'
+$Script:REG_USER_ENV           = "$PSScriptRoot\Environment\UserEnv.reg"
+$Script:REG_GLOBAL_ENV         = "$PSScriptRoot\Environment\GlobalEnv.reg"
+$Script:OrganizationHKCUScript = "$PSScriptRoot\Registry\OrganizationHKCU.reg"
+
+$Script:GIT_WINDOWS_64_URL      = 'https://github.com/git-for-windows/git/releases/download/v2.35.1.windows.2/Git-2.35.1.2-64-bit.exe'
+$Script:PROGRAMS_PATH           = 'c:\Programs'
+$Script:WIN_GIT_INSTALL_PATH    = Join-Path "$Script:PROGRAMS_PATH" "Git"
+$Script:MYDOCUMENTS_PATH        = 'c:\DOCUMENTS'
+$Script:MYPICTURES_PATH         = 'c:\Data\Pictures'
+$Script:MYVIDEOS_PATH           = 'c:\Data\Videos'
+$Script:SCREENSHOTS_PATH        = 'c:\Data\Pictures\Screenshots'
+$Script:DOWNLOAD_PATH           = 'c:\Data\Downloads' 
+$Script:DESKTOP_PATH            = 'c:\Data\Windows\Desktop' 
+
+$Script:POWERSHELL_PATH         = Join-Path "$Script:MYDOCUMENTS_PATH" "PowerShell"
+$Script:PS_MODULES_PATH         = Join-Path "$Script:POWERSHELL_PATH" "Modules"
+$Script:PS_MODDEV_PATH          = Join-Path "$Script:POWERSHELL_PATH" "Modules-Development"
+$Script:PS_PROFILE_PATH         = Join-Path "$Script:POWERSHELL_PATH" "Profile"
+$Script:PS_PROJECTS_PATH        = Join-Path "$Script:POWERSHELL_PATH" "Projects"
+
+$Script:GITHUB_AUTH_USER        = 'arsscriptum'
+$Script:GITHUB_AUTH_TOKEN       = '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'
+
+
+
+if (-not $env:TEMP) {
+    $env:TEMP = Join-Path $env:SystemDrive -ChildPath 'temp'
+}
+
+$Script:TEMP_INSTALL_PATH = Join-Path $env:TEMP -ChildPath "SystemConfiguration"
+
+
+function Invoke-GitClone {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param
+    (
+        [Parameter(Mandatory=$true,Position=0)]     
+        [string]$Repo,
+        [Parameter(Mandatory=$false)]     
+        [string]$Name
+    )
+
+    $GitExe = (get-command 'git.exe').Source
+
+    if ($PSBoundParameters.ContainsKey('Name')) {
+        Write-Host '[GIT] ' -f DarkRed -NoNewLine
+        Write-Host "clone $Repo.git" -f Yellow            
+        &"$GitExe" "clone" "https://$Script:GITHUB_AUTH_USER:$Script:GITHUB_AUTH_TOKEN@github.com/$Script:GITHUB_AUTH_USER/$Repo.git" "$Name"
+    }else{
+        &"$GitExe" "clone" "https://$Script:GITHUB_AUTH_USER:$Script:GITHUB_AUTH_TOKEN@github.com/$Script:GITHUB_AUTH_USER/$Repo.git"    
+    }  
+    
+    
+}
+
+
+function write-slog {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param
+    (
+        [Parameter(Mandatory=$true,Position=0)]     
+        [string]$Message
+    )
+    Write-Host -n -f DarkCyan "[i] "
+    Write-Host -f Cyan "$Message"  
+}
+
+function write-serror {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param
+    (
+        [Parameter(Mandatory=$true,Position=0)]     
+        [string]$Message
+    )
+    Write-Host -n -f DarkRed "[ERROR] "
+    Write-Host -f DarkYellow "$Message"  
+}
+
+
+function ValidateBeforeStart {
+
+    if(-not(Test-Path -Path $Script:DEVDRIVE))              { write-serror "NOT EXISTING: $Script:DEVDRIVE" }
+    if(-not(Test-Path -Path $Script:REG_USER_ENV))          { write-serror "NOT EXISTING: $Script:REG_USER_ENV" }
+    if(-not(Test-Path -Path $Script:REG_GLOBAL_ENV))        { write-serror "NOT EXISTING: $Script:REG_GLOBAL_ENV" }
+    if(-not(Test-Path -Path $Script:OrganizationHKCUScript)){ write-serror "NOT EXISTING: $Script:OrganizationHKCUScript" }
+}
+
+function Get-OnlineFileNoCache{
+    [CmdletBinding(SupportsShouldProcess=$true)]
+    param(
+        [Parameter(Mandatory=$true,Position=0)]   
+        [string]$Url,
+        [Parameter(Mandatory=$true,Position=1)]   
+        [string]$Path,
+        [Parameter(Mandatory=$false)]
+        [string]$ProxyAddress,
+        [Parameter(Mandatory=$false)]
+        [string]$ProxyUser,
+        [Parameter(Mandatory=$false)]
+        [string]$ProxyPassword,
+        [Parameter(Mandatory=$false)]
+        [string]$UserAgent=""
+    )
+
+    $ForceNoCache=$True
+
+    $client = New-Object Net.WebClient
+    if( $PSBoundParameters.ContainsKey('ProxyAddress') ){
+        Write-Warning ('NetGetFileNoCache''s -ProxyAddress parameter is not tested.')
+        $proxy = New-object System.Net.WebProxy "$ProxyAddress"
+        $proxy.Credentials = New-Object System.Net.NetworkCredential ($ProxyUser, $ProxyPassword) 
+        $client.proxy=$proxy
+    }
+    
+    if($UserAgent -ne ""){
+        $Client.Headers.Add("user-agent", "$UserAgent")     
+    }else{
+        $Client.Headers.Add("user-agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1") 
+    }
+
+    $RequestUrl = "$Url"
+
+    if ($ForceNoCache) {
+        # doesn’t use the cache at all
+        $client.CachePolicy = New-Object Net.Cache.RequestCachePolicy([Net.Cache.RequestCacheLevel]::NoCacheNoStore)
+
+        $RandId=(new-guid).Guid
+        $RandId=$RandId -replace "-"
+        $RequestUrl = "$Url" + "?id=$RandId"
+    }
+    Write-Host "NetGetFileNoCache: Requesting $RequestUrl"
+    $client.DownloadFile($RequestUrl,$Path)
+}
+
+
+
+
+function Install-WindowsGit {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param
+    (
+        [Parameter(Mandatory=$true,Position=0)]    
+        [string]$Url,
+        [Parameter(Mandatory=$true,Position=1)]    
+        [string]$InstallPath
+    )
+    $Installed =  Test-GitInstalled
+    if($Installed) {write-slog "Windows Git already installed..." ; return ; } 
+    write-slog "Installing Windows Git from $Url to $InstallPath"
+
+    $LocalInstaller = Join-Path "$ENV:Temp" "Git.exe"
+    Get-OnlineFileNoCache -Url "$Url" -Path $LocalInstaller
+    if(-not(Test-Path $LocalInstaller)){
+        throw "Failed to get Git installer"
+    }
+
+    write-slog "$LocalInstaller /NORESTART /SUPPRESSMSGBOXES /VERYSILENT /SP /ALLUSERS /NOCANCEL /NOCLOSEAPPLICATIONS /DIR=$InstallPath"
+
+    # Args: https://jrsoftware.org/ishelp/index.php?topic=setupcmdline
+    &"$LocalInstaller" "/NORESTART" "/SUPPRESSMSGBOXES" "/VERYSILENT" "/SP" "/ALLUSERS" "/NOCANCEL" "/NOCLOSEAPPLICATIONS" "/DIR=`"$InstallPath`""
+}
+
+
+
+function Test-GitInstalled {
+    [CmdletBinding()]
+    param()
+
+    $checkPath = 'c:\Programs\Git'
+
+    if ($Command = Get-Command 'git.exe' -CommandType Application -ErrorAction Ignore) {
+        Write-Verbose "git is on the PATH, assume it's installed"
+        $true
+    }
+    elseif (-not (Test-Path $checkPath)) {
+        Write-Verbose "Install folder doesn't exist"
+        $false
+    }
+    elseif (-not (Get-ChildItem -Path $checkPath)) {
+        Write-Verbose "Install folder exists but is empty"
+        $false
+    }
+    else {
+        Write-Verbose "Install folder exists and is not empty"
+
+        $checkFiles = @('c:\Programs\Git\git-bash.exe', 'c:\Programs\Git\git-cmd.exe', 'c:\Programs\Git\bin\git.exe', 'c:\Programs\Git\bin\sh.exe', 'c:\Programs\Git\bin\bash.exe')
+        
+        ForEach($theFile in $checkFiles){
+            if (-not (Test-Path $theFile)) {
+                Write-Verbose "$theFile missing"
+                return $false
+            }
+        }
+        $true
+    }
+}
+
+function Wait-GitInstalled {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param
+    ()
+    
+    $GitInstalled = $false
+
+    While($GitInstalled -eq $False){
+        Sleep -Seconds 5
+        Write-Verbose "Checking if Git is Installed....."
+        $GitInstalled = Test-GitInstalled
+    }
+
+    Write-Verbose "Git is Installed"
+}
+
+
+
+function Set-HiddenDriveP {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param
+    ()
+    write-slog "Set the P drive has Hidden"
+    $Null = New-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer' NoDrives -Value 32768 -Type DWORD -Force
+}
+
+
+function Install-Chocolatey{
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param
+    ()
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+
+
+function Set-SystemEnvironmentValues {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param
+    ()
+
+    $RegExe = (get-command 'reg.exe').Source
+    $UserEnvScript = "$PSScriptRoot\Environment\UserEnv.reg"
+    $GlobalEnvScript = "$PSScriptRoot\Environment\GlobalEnv.reg"
+
+    if(-not(Test-Path -Path $GlobalEnvScript)){ throw "Missing $GlobalEnvScript" }
+    if(-not(Test-Path -Path $UserEnvScript)){ throw "Missing $UserEnvScript" }
+
+    write-slog "Registry Import $GlobalEnvScript"
+    &"$RegExe" "import" "$GlobalEnvScript"
+    write-slog "Registry Import $UserEnvScript"
+    &"$RegExe" "import" "$UserEnvScript"
+
+}
+
+
+
+function Set-WellKnownPaths {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param
+    ()
+
+    Write-Host "======================================================"
+    Write-Host "WELL KNOWN PATHS (DOCUMENTS, PICTURES, ETC)"
+    Write-Host "======================================================"
+
+    Write-Host "Desktop:         $Script:DESKTOP_PATH"
+    Write-Host "Pictures:        $Script:MYPICTURES_PATH"
+    Write-Host "Screenshots:     $Script:SCREENSHOTS_PATH"
+    Write-Host "Download:        $Script:DOWNLOAD_PATH"
+    Write-Host "Documents:       $Script:MYDOCUMENTS_PATH"
+    Write-Host "Videos:          $Script:MYVIDEOS_PATH"
+
+    $confirm = Read-Host -Prompt 'Are all the paths above correct (y/n)'
+    if ($confirm -ne 'y') {
+        Write-Host -n -f DarkRed "[!] "
+        Write-Host -f DarkYellow "Edit the script to set the values you want."
+        return
+    }
+
+    # MY DOCUMENTS
+    New-Item -Path "$Script:DESKTOP_PATH" -ItemType Directory -Force -ErrorAction Ignore | Out-Null
+    New-Item -Path "$Script:MYPICTURES_PATH" -ItemType Directory -Force -ErrorAction Ignore | Out-Null
+    New-Item -Path "$Script:SCREENSHOTS_PATH" -ItemType Directory -Force -ErrorAction Ignore | Out-Null
+    New-Item -Path "$Script:DOWNLOAD_PATH" -ItemType Directory -Force -ErrorAction Ignore | Out-Null
+    New-Item -Path "$Script:MYDOCUMENTS_PATH" -ItemType Directory -Force -ErrorAction Ignore | Out-Null
+    New-Item -Path "$Script:MYVIDEOS_PATH" -ItemType Directory -Force -ErrorAction Ignore | Out-Null
+    New-Item -Path "$Script:POWERSHELL_PATH" -ItemType Directory -Force -ErrorAction Ignore | Out-Null
+
+    # POWERSHELL PROFILE
+    New-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders' Personal -Value "$Script:MYDOCUMENTS_PATH" -Type ExpandString -Force | Out-Null
+    Set-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders' Desktop -Value "$Script:DESKTOP_PATH" -Type ExpandString -Force | Out-Null
+    Set-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders' 'My Pictures' -Value "$Script:MYPICTURES_PATH" -Type ExpandString -Force | Out-Null
+    Set-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders' 'My Video' -Value "$Script:MYVIDEOS_PATH" -Type ExpandString -Force | Out-Null
+}
+
+
+function Set-PowerShellPaths {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param
+    ()
+
+    Write-Host "======================================================"
+    Write-Host "SET POWERSHELL HOME, MODULES, MODULE DEVELOPMENT PATH"
+    Write-Host "======================================================"
+
+    Write-Host "PowerShell Home:   $Script:POWERSHELL_PATH"
+    Write-Host "Modules:           $Script:PS_MODULES_PATH"
+    Write-Host "Modules Dev:       $Script:PS_MODDEV_PATH"
+    Write-Host "Projects:          $Script:PS_PROJECTS_PATH"
+    Write-Host "Profile:           $Script:PS_PROFILE_PATH"
+
+    $confirm = Read-Host -Prompt 'Are all the paths above correct (y/n)'
+    if ($confirm -ne 'y') {
+        Write-Host -n -f DarkRed "[!] "
+        Write-Host -f DarkYellow "Edit the script to set the values you want."
+        return
+    }
+
+    New-Item -Path "$Script:POWERSHELL_PATH" -ItemType Directory -Force -ErrorAction Ignore | Out-Null
+    New-Item -Path "$Script:PS_MODULES_PATH" -ItemType Directory -Force -ErrorAction Ignore | Out-Null
+    New-Item -Path "$Script:PS_MODDEV_PATH" -ItemType Directory -Force -ErrorAction Ignore | Out-Null
+    New-Item -Path "$Script:PS_PROJECTS_PATH" -ItemType Directory -Force -ErrorAction Ignore | Out-Null
+    New-Item -Path "$Script:PS_PROFILE_PATH" -ItemType Directory -Force -ErrorAction Ignore | Out-Null
+}
+
+
+function Invoke-InstallWindowsGit {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param
+    ()
+
+    Write-Host "======================================================"
+    Write-Host "INSTALL WIN GIT"
+    Write-Host "======================================================"
+
+    Write-Host "GIT Install Url :   $Script:GIT_WINDOWS_64_URL"
+    Write-Host "Git Install Path:   $Script:WIN_GIT_INSTALL_PATH"
+
+    $confirm = Read-Host -Prompt 'Install Windows Git ? (y/n)'
+    if ($confirm -ne 'y') {
+         Write-Host -n -f DarkRed "[!] "
+        Write-Host -f DarkYellow " Exit. "
+        return
+    }
+    Install-WindowsGit -Url "$Script:GIT_WINDOWS_64_URL" -InstallPath "$Script:WIN_GIT_INSTALL_PATH"
+}
+
+function Clone-BaseRepos {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param
+    ()
+
+    New-Item -Path "$Script:DEV_ROOT" -ItemType Directory -Force -ErrorAction Ignore | Out-Null
+
+    pushd "$Script:DEV_ROOT"
+
+    Invoke-GitClone 'BuildAutomation'
+
+    Invoke-GitClone 'DejaInsight'
+    popd
+
+}
+
+
+function Clone-PwshRepos {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param
+    ()
+    
+    write-slog "Going in $ENV:TEMP"
+    pushd "$ENV:TEMP"
+    write-slog "Cloning PowerShell.Profile"
+    Invoke-GitClone 'PowerShell.Profile'
+    pushd 'PowerShell.Profile'
+    write-slog "COPY Microsoft.PowerShell_profile.ps1 --> to $Script:POWERSHELL_PATH"
+    Copy-Item "Microsoft.PowerShell_profile.ps1" "$Script:POWERSHELL_PATH"
+
+    $Null=Remove-Item -Path "$Script:POWERSHELL_PATH\Profile" -Recurse -Force -ErrorAction Ignore
+    Copy-Item "Profile" "$Script:POWERSHELL_PATH\Profile" -Recurse 
+}
+
+function Clone-PwshModules {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param
+    ()
+
+    pushd "$Script:PS_PROJECTS_PATH"
+    Invoke-GitClone 'PowerShell.ModuleBuilder'
+    pushd "PowerShell.ModuleBuilder\setup"
+    ./Setup.ps1
+    popd
+
+
+    pushd "$Script:PS_MODDEV_PATH"
+
+    Invoke-GitClone 'PowerShell.Module.Core'
+    Invoke-GitClone 'PowerShell.Module.Reddit'
+    Invoke-GitClone 'PowerShell.Module.Github'
+    Invoke-GitClone 'PowerShell.Module.WindowsHost'
+    popd
+
+}
+
+
+
+function Set-RegistryOrganizationHKCU {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param
+    ()
+
+    #===============================================================================
+    # OrganizationHKCU
+    #===============================================================================
+    if( ($ENV:OrganizationHKCU -eq $null) -Or ($ENV:OrganizationHKCU -eq '') )
+    {
+        Write-Host "===============================================================================" -f DarkRed    
+        Write-Host "A required environment variable needs to be setup (user scope)     `t" -NoNewLine -f DarkYellow ; Write-Host "$Script:OrganizationHKCU" -f Gray 
+        $OrgIdentifier = "_gp"
+        $OrganizationHKCU = "HKCU:\Software\" + "$OrgIdentifier"
+
+        [Environment]::SetEnvironmentVariable('OrganizationHKCU',$OrganizationHKCU,"User")
+
+        Write-Host "Setting OrganizationHKCU --> $OrganizationHKCU [User]"  -ForegroundColor Yellow
+        $Null = New-Item -Path "$OrganizationHKCU" -ItemType Directory -Force -ErrorAction Ignore
+
+        $Cmd = Get-Command "RefreshEnv.cmd"
+        if($Cmd){
+            $RefreshEnv = $Cmd.Source
+            &"$RefreshEnv"
+        }
+
+        $ENV:OrganizationHKCU = "$OrganizationHKCU"
+
+    }
+}
+
+
+
+
+
+
+
+
+ValidateBeforeStart 
+
+
+Set-RegistryOrganizationHKCU
+
+
+Read-Host 'Next: Set-SystemEnvironmentValues'
+Set-SystemEnvironmentValues
+
+Read-Host 'Next: Set-WellKnownPaths'
+Set-WellKnownPaths
+
+Read-Host 'Next: Set-PowerShellPaths'
+Set-PowerShellPaths
+
+
+Read-Host 'Next: Invoke-InstallWindowsGit'
+Invoke-InstallWindowsGit
+Wait-GitInstalled 
+
+git config --global user.email "guillaumeplante.qc@gmail.com"
+git config --global user.name "Guillaume Plante"
+
+
+Clone-BaseRepos
+
+Clone-PwshRepos
+
+
+Clone-PwshModules
+
+
+Read-Host 'Next: Install-Chocolatey'
+Install-Chocolatey
---
+++ NewInstallation/StandaloneScripts/Github.ps1
@@ -0,0 +1,51 @@
+# MY DOCUMENTS
+New-Item -Path 'c:\DOCUMENTS' -ItemType Directory -Force -ErrorAction Ignore | Out-Null
+New-Item -Path 'c:\Data\Pictures\Screenshots' -ItemType Directory -Force -ErrorAction Ignore | Out-Null
+New-Item -Path 'c:\Data\Downloads' -ItemType Directory -Force -ErrorAction Ignore | Out-Null
+New-Item -Path 'c:\Data\Windows\Desktop' -ItemType Directory -Force -ErrorAction Ignore | Out-Null
+
+# POWERSHELL PROFILE
+New-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders' Personal -Value 'c:\DOCUMENTS' -Type ExpandString -Force | Out-Null
+
+Set-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders' Desktop -Value 'c:\Data\Windows\Desktop' -Type ExpandString -Force | Out-Null
+Set-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders' 'My Pictures' -Value 'c:\Data\Pictures' -Type ExpandString -Force | Out-Null
+Set-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders' 'My Video' -Value 'c:\Data\Videos' -Type ExpandString -Force | Out-Null
+
+
+# Install the OpenSSH Client
+Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+
+# Install the OpenSSH Server
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+
+# Start the sshd service
+Start-Service sshd
+
+# OPTIONAL but recommended:
+Set-Service -Name sshd -StartupType 'Automatic'
+
+# Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
+if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+    Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
+    New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+} else {
+    Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
+}
+
+choco install pasteboard
+Set-Service -Name "ssh-agent" -StartupType Automatic
+Start-Service -Name "ssh-agent"
+ssh-keygen -t rsa -b 4096 -C "cybercastor@icloud.com" -g -f "C:\Users\radic\.ssh\id_cybercastor"
+ssh-keygen -t rsa -b 4096 -C "radicaltronic@gmail.com" -g -f "C:\Users\radic\.ssh\id_radicaltronic"
+ssh-keygen -t rsa -b 4096 -C "zolaternative@icloud.com" -g -f "C:\Users\radic\.ssh\id_anonymous"
+ssh-keygen -t rsa -b 4096 -C "guillaumeplante.qc@gmail.com" -g -f "C:\Users\radic\.ssh\id_arsscriptum"
+# CONFIGURE GITHUB ACCOUNT: cybercastor
+# type "C:\Users\radic\.ssh\id_cybercastor.pub" | pbcopy.exe  
+# CONFIGURE GITHUB ACCOUNT: radicaltronic
+# type "C:\Users\radic\.ssh\id_radicaltronic.pub" | pbcopy.exe  
+# CONFIGURE GITHUB ACCOUNT: vcvzsds7868745 
+# type "C:\Users\radic\.ssh\id_anonymous.pub" | pbcopy.exe   
+# CONFIGURE GITHUB ACCOUNT: vcvzsds7868745 
+# type "C:\Users\radic\.ssh\id_arsscriptum.pub" | pbcopy.exe  
+
+
